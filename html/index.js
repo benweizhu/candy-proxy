a52dc50b1bd6a25f6d72a4e13441af24aeb7f5ef
Benwei Zhu <benweizhu@gmail.com>
2018-11-29 21:44:36 +0800

    add backend server mock

--- html/index.js
+++ html/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 5000
 
-app.get('/api/user', (req, res) => res.send({name: 'Benwei'}))
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
